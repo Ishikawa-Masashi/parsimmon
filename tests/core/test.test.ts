de31e885536a673443eb0eed82968a17a207e1cd
IshikawaMasashi <IshikawaMasashi@outlook.com>
2023-02-05 06:00:39 +0900

    fix

--- tests/core/test.test.ts
+++ tests/core/test.test.ts
@@ -2,7 +2,7 @@ import { it, expect } from 'vitest';
 import * as Parsimmon from '../../src/parsimmon';
 
 it('test', () => {
-  const parser = Parsimmon.test(function (ch) {
+  const parser = Parsimmon.test((ch) => {
     return ch !== '.';
   });
   // assert.equal(parser.parse("x").value, "x");
@@ -12,7 +12,7 @@ it('test', () => {
   // assert.throws(function() {
   //   Parsimmon.test("not a function");
   // });
-  expect(function () {
+  expect(() => {
     Parsimmon.test('not a function');
   }).toThrow();
 });
